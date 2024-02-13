a4ce40e6efa440db96a8c4d90d93d006020fbf29
WizardlyBump17 <davicraft130@gmail.com>
2024-02-13 00:31:18 -0300

    deleted the unused Pair class

--- src/main/java/com/wizardlybump17/commands/pair/Pair.java
+++
@@ -1,8 +0,0 @@
-package com.wizardlybump17.commands.pair;
-
-public record Pair<F, S>(F first, S second) {
-
-    public static <F, S> Pair<F, S> of(F first, S second) {
-        return new Pair<>(first, second);
-    }
-}
